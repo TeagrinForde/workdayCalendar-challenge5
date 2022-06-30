d1f91aa8eb9ea9b2d4589a40afe76f2f0f91b90d
Teagrin Forde <teagrinb@gmail.com>
2022-06-30 10:01:02 -0400

    Display Time in js

---
+++ index.js
@@ -0,0 +1,9 @@
+var displayTimeEl = $('#currentDay');
+var day = $('#calendarDay');
+
+function displayTime() {
+    var today = moment().format("MMM Do, YYYY");
+    displayTimeEl.text(today);
+};
+displayTime();
+
